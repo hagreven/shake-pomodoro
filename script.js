09ef64aae95de08b84ab66e487497d15ed13c3d9
Hannah Greven <ultfj@student.kit.edu>
2021-04-28 10:59:05 +0200

    Call by reference

--- script.js
+++ script.js
@@ -177,9 +177,9 @@ function move(val, label, max){
 }
 
 function showScore(){
-    move(laSensor.x, "xBar", "xMax"); // passing variable reference instead of value
-    move(laSensor.y, "yBar", "yMax"); // passing variable reference instead of value
-    move(laSensor.z, "zBar", "zMax"); // passing variable reference instead of value
+    move(laSensor.x, "xBar", this.xMax); // passing variable reference instead of value
+    move(laSensor.y, "yBar", this.yMax); // passing variable reference instead of value
+    move(laSensor.z, "zBar", this.zMax); // passing variable reference instead of value
     
 }
 
